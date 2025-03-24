415c17344653c5570b0d7a7d5d95e1613968b0ce
PurpleAlot <140986211+PurpleAlot@users.noreply.github.com>
2025-03-24 13:12:40 -0700

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BepInEx;
+using GorillaNetworking;
+using StartupRoomJoiner.CustomCode;
+using static StartupRoomJoiner.CustomCode.Configmanager;
+using UnityEngine;
+using Utilla;
+
+namespace StartupRoomJoiner
+{
+
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+	{
+		string codetoJoin = Configmanager.codetoJoin.Value;
+
+        void Start()
+		{
+            Configmanager.CreateConfig();
+            
+
+            Utilla.Events.GameInitialized += OnGameInitialized;
+		}
+
+
+		void OnGameInitialized(object sender, EventArgs e)
+		{
+            PhotonNetworkController.Instance.AttemptToJoinSpecificRoom(codetoJoin, JoinType.Solo);
+        }
+	}
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace StartupRoomJoiner
+{ 
+	class PluginInfo
+	{
+		public const string GUID = "com.purplealot.gorillatag.startuproomjoiner";
+		public const string Name = "StartupRoomJoiner";
+		public const string Version = "1.0.0";
+	}
+}
